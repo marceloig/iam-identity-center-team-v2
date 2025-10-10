db1e25f8282f75a79f3ee5366a5fc901140fc6d0
Igor Oliveira <cccmigor@gmail.com>
2025-10-09 22:57:10 -0300

    feat: initialize team project with TypeScript, AWS CDK, and Jest
    - Added package.json with scripts for build, watch, test, and CDK commands.
    - Included devDependencies for TypeScript, Jest, and AWS CDK.
    - Created initial test file for team stack with a placeholder test for SQS Queue.
    - Configured tsconfig.json with strict type checking and ES2022 target.

---
+++ deployment/team/lib/team-stack.ts
@@ -0,0 +1,134 @@
+import * as cdk from 'aws-cdk-lib';
+import * as amplify from 'aws-cdk-lib/aws-amplify';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { Construct } from 'constructs';
+
+export interface TeamStackProps extends cdk.StackProps {
+  login: string;
+  cloudTrailAuditLogs: string;
+  teamAdminGroup: string;
+  teamAuditGroup: string;
+  teamTags?: string;
+  teamAccount: string;
+  customAmplifyDomain?: string;
+  customRepository?: true | false;
+  customRepositorySecretName?: string;
+}
+
+export class TeamStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: TeamStackProps) {
+    super(scope, id, props);
+
+    // Amplify Service Role
+    const amplifyRole = new iam.Role(this, 'AmplifyRole', {
+      assumedBy: new iam.CompositePrincipal(
+        new iam.ServicePrincipal('amplify.amazonaws.com')
+      ).withConditions({
+        StringEquals: {
+          'aws:SourceAccount': this.account
+        }
+      }),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AdministratorAccess')
+      ]
+    });
+
+    // Amplify App
+    const amplifyApp = new amplify.CfnApp(this, 'AmplifyApp', {
+      name: 'TEAM-IDC-APP',
+      repository: `{{resolve:secretsmanager:${props.customRepositorySecretName}:SecretString:url}}`,
+      accessToken: `{{resolve:secretsmanager:${props.customRepositorySecretName}:SecretString:AccessToken}}`,
+      description: 'Temporary Elevated Access Management Application',
+      customRules: [
+        {
+          source: '/<*>',
+          status: '404',
+          target: '/index.html'
+        },
+        {
+          source: '</^[^.]+$|\\.(?!(css|gif|ico|jpg|js|png|txt|svg|woff|ttf|map|json)$)([^.]+$)/>',
+          status: '200',
+          target: '/index.html'
+        }
+      ],
+      environmentVariables: [
+        { name: 'AMPLIFY_DESTRUCTIVE_UPDATES', value: 'true' },
+        { name: '_LIVE_UPDATES', value: '[{"name":"Node.js version","pkg":"node","type":"nvm","version":"22.17"}]' }
+      ],
+      buildSpec: `
+      version: 1
+      backend:
+        phases:
+          build:
+            commands:
+              - npm install --cache .npm --prefer-offline
+              - npx ampx pipeline-deploy --branch $AWS_BRANCH --app-id $AWS_APP_ID
+      frontend:
+        phases:
+          preBuild:
+            commands:
+              - npm install
+          build:
+            commands:
+              - npm run build
+        artifacts:
+          baseDirectory: build
+          files:
+            - '**/*'
+        cache:
+          paths:
+            - node_modules/**/*`,
+      tags: [{ key: 'Name', value: 'TEAM' }],
+      iamServiceRole: amplifyRole.roleArn
+    });
+
+    // Amplify Branch
+    const amplifyBranch = new amplify.CfnBranch(this, 'AmplifyBranch', {
+      appId: amplifyApp.attrAppId,
+      branchName: 'main',
+      enableAutoBuild: true,
+      environmentVariables: [
+        { name: 'SSO_LOGIN', value: props.login },
+        { name: 'TEAM_ACCOUNT', value: props.teamAccount },
+        { name: 'CLOUDTRAIL_AUDIT_LOGS', value: props.cloudTrailAuditLogs },
+        { name: 'TEAM_ADMIN_GROUP', value: props.teamAdminGroup },
+        { name: 'TEAM_AUDITOR_GROUP', value: props.teamAuditGroup },
+        { name: 'TAGS', value: props.teamTags || '' },
+        { name: 'AMPLIFY_CUSTOM_DOMAIN', value: props.customAmplifyDomain || '' },
+      ],
+      tags: [{ key: 'Branch', value: 'main' }]
+    });
+
+    // Lambda Role for Amplify Build Trigger
+    const amplifyLambdaRole = new iam.Role(this, 'AmplifyLambdaRole', {
+      assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
+      inlinePolicies: {
+        AmplifyLambdaPolicy: new iam.PolicyDocument({
+          statements: [
+            new iam.PolicyStatement({
+              sid: 'AllowLogging',
+              effect: iam.Effect.ALLOW,
+              actions: [
+                'logs:CreateLogGroup',
+                'logs:CreateLogStream',
+                'logs:PutLogEvents'
+              ],
+              resources: ['*']
+            }),
+            new iam.PolicyStatement({
+              sid: 'startBuild',
+              effect: iam.Effect.ALLOW,
+              actions: ['amplify:StartJob'],
+              resources: ['*']
+            })
+          ]
+        })
+      }
+    });
+
+    // Output
+    new cdk.CfnOutput(this, 'DefaultDomain', {
+      value: amplifyApp.attrDefaultDomain
+    });
+  }
+}
---
+++ deployment/team/test/team.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Team from '../lib/team-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/team-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Team.TeamStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
